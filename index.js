a131f250a981d0ccde053a871d014fc0434548c1
sumitbadri2000 <sumitbadri2000@gmail.com>
2023-02-20 17:50:07 +0530

    deployment

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const { auth } = require("./middleware/Authentication");
 const { postR } = require("./routes/post.routes");
 const { userR } = require("./routes/user.routes");
 const app = express();
-require("dotenv").config();
 
 app.use(express.json());
 
@@ -14,12 +13,12 @@ app.get("/", (req, res) => {
 app.use("/users", userR);
 app.use(auth);
 app.use("/posts", postR);
-app.listen(process.env.port, async () => {
+app.listen(1000, async () => {
   try {
     await connection;
     console.log("server");
   } catch (error) {
     console.log(error);
   }
-  console.log(process.env.port);
+  console.log("1000");
 });
